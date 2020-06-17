f1cc340295d21d88463dbaaa746ba17e4e166666
parkty <parkty@lgcns.com>
2020-06-17 19:49:13 +0900

    File IO 패턴 개선

--- src/javaasp/sp/pattern/fileio/FileIONObject.java
+++ src/javaasp/sp/pattern/fileio/FileIONObject.java
@@ -1,10 +1,16 @@
 package javaasp.sp.pattern.fileio;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 패턴
@@ -17,32 +23,58 @@ public class FileIONObject {
 
 	public static void main(String[] args) throws Exception {
 		
-		//파일 읽기
-		Scanner fs = new Scanner(new File("./READFILE.TXT"));
-		//사용자 입력 읽기
-		//Scanner fs = new Scanner(System.io);
+		String filePath = "./READFILE.TXT";
+		List<FileLineObject> objList = null;
 		
-		//라인 단위 객체화(특정 문자로 문자 자르기)
-		ArrayList<FileLineObject> lineList = new ArrayList<FileLineObject>();
-		//라인 단위 처리
-		while(fs.hasNextLine()) {
-			String fileLine = fs.nextLine();
-			//문자열 처리 : StringTokenizer, Split(정규식), Matcher(정규식), substring(고정길이경우)
-			String[] lineDatas = fileLine.split(",");
-			lineList.add(new FileLineObject(lineDatas[0], lineDatas[1], Integer.parseInt(lineDatas[2])));
-		}
+		//1.1 파일 라인 단위로 읽어 라인별 객체화 하기 - 방법 1 : Scanner
+		objList = getObjecListByFileLineWithScanner(filePath);
 		
+		//1.2 파일 라인 단위로 읽어 라인별 객체화 하기 - 방법 2 : Stream
+		objList = getObjectListByFileLineWithStream(filePath);
+				
 		//TODO : 데이터 처리
-		for(FileLineObject oneObject : lineList) {
+		for(FileLineObject oneObject : objList) {
 			//TODO : 라인 별 추가 작업
 		}
 
 		//처리 결과 파일 쓰기
 		PrintWriter printWriter = new PrintWriter(new File("./WRITEFILE.TXT"));
-		for(FileLineObject oneObject : lineList) {
+		//3.1 for문 활용 출력
+		for(FileLineObject oneObject : objList) {
 			printWriter.println(oneObject);
 		}
+		//3.2 stream 활용 출력
+		//objList.stream().forEach(o->printWriter.println(o));
+		
 		printWriter.close();
+	}
+	
+	public static List<FileLineObject> getObjecListByFileLineWithScanner(String filePath) throws IOException {
+		Scanner fs = new Scanner(new File(filePath));
+		//사용자 입력 읽기
+		//Scanner fs = new Scanner(System.io);
+		
+		//라인 단위 객체화(특정 문자로 문자 자르기)
+		ArrayList<FileLineObject> objList = new ArrayList<FileLineObject>();
+		//라인 단위 처리
+		while(fs.hasNextLine()) {
+			String fileLine = fs.nextLine();
+			//문자열 처리 : StringTokenizer, Split(정규식), Matcher(정규식), substring(고정길이경우)
+			String[] lineDatas = fileLine.split(",");
+			objList.add(new FileLineObject(lineDatas[0], lineDatas[1], Integer.parseInt(lineDatas[2])));
+		}
+		fs.close();
+		
+		return objList;
+	}
+
+	public static List<FileLineObject> getObjectListByFileLineWithStream(String filePath) throws IOException {
+		List<FileLineObject> objList = null;
+		try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+			objList = stream.map(line->new FileLineObject(line)).collect(Collectors.toList());
+		}
+		
+		return objList;
 	}
 }
 
@@ -55,6 +87,14 @@ class FileLineObject implements Comparable<FileLineObject> {
 	String name;
 	int age;
 
+	public FileLineObject(String parseLine) {
+		String[] lineDatas = parseLine.split(",");
+		
+		id = lineDatas[0];
+		name = lineDatas[1];
+		age = Integer.parseInt(lineDatas[2]);
+	}
+	
 	public FileLineObject(String id, String name, int age) {
 		super();
 		this.id = id;
