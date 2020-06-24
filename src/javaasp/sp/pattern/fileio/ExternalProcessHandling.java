957cdc0c2bd3e424e41fb47f699f4b3bae01eb93
parkty <parkty@lgcns.com>
2020-06-24 11:26:54 +0900

    외부프로그램 입출력 함수화 패턴

---
+++ src/javaasp/sp/pattern/fileio/ExternalProcessHandling.java
@@ -0,0 +1,45 @@
+package javaasp.sp.pattern.fileio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ExternalProcessHandling {
+	
+	public static void main(String[] args) throws Exception {
+		List<String> lineDatas = ExternalProcessHandling.readFromExternalProcess("tasklist");
+		
+		//TODO : 데이터 처리
+		
+		ExternalProcessHandling.writeToExternalProcess("./writeprogram.exe", lineDatas);
+	}
+	
+	
+	public static List<String> readFromExternalProcess(String command) throws Exception {
+		Process theProcess = Runtime.getRuntime().exec(command);
+	    BufferedReader inStream = new BufferedReader(new InputStreamReader( theProcess.getInputStream(),"euc-kr"));
+	    
+	    List<String> readData = new ArrayList<String>();
+	    String line = null;
+	    while ( ( line = inStream.readLine( ) ) != null ) {
+	    	readData.add(line);
+	    }
+	    
+	    return readData;		
+	}
+	
+	public static void writeToExternalProcess(String command, List<String> writeData) throws Exception {
+		Process theProcess = Runtime.getRuntime().exec(command);
+		PrintWriter printWriter = new PrintWriter(theProcess.getOutputStream(), true);
+		//BufferedWriter outStream = new BufferedWriter(new OutputStreamWriter(theProcess.getOutputStream(),"euc-kr"));
+	    
+		for(String oneData : writeData) {
+			printWriter.println(oneData);
+		}
+	    
+		printWriter.close();	
+	}
+}
